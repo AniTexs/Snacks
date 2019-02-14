f36e40e1e9edf77672ee46b54dd73dd0427de48b
Martin Sonne <mart605h@elevcampus.dk>
2019-02-14 13:15:40 +0100

    jhgkjhg

--- Program.cs
+++ Program.cs
@@ -10,6 +10,20 @@ namespace Snacks
     {
         static void Main(string[] args)
         {
+            Donut nyDonut = new Donut("Kakao Donut", 12, 100);
+        }
+    }
+    class Donut
+    {
+        public string DonutType { get; set; }
+        public int DonutPrice { get; set; }
+        public int DonutAmount { get; set; }
+
+        public Donut(string DonutType, int DonutPrice, int DonutAmount)
+        {
+            this.DonutType = DonutType;
+            this.DonutPrice = DonutPrice;
+            this.DonutAmount = DonutAmount;
         }
     }
 }
